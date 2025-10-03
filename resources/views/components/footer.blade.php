2e0d699773fb785042ca8fea19550e96b34b8e27
Michelangelo Ruisi <mruisi96@gmail.com>
2025-10-03 16:10:21 +0200

    elementi UI finiti

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-dark text-light py-4">
+  <div class="container text-center">
+    <p class="mb-1">&copy; 2025 Presto.it. Tutti i diritti riservati.</p>
+    <a href="#" class="text-light me-3">Privacy Policy</a>
+    <a href="#" class="text-light">Contatti</a>
+  </div>
+</footer>
