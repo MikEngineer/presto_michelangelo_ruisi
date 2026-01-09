dcd22878ca5286e36029db1aeef48e0ad77c0756
Michelangelo Ruisi <mruisi96@gmail.com>
2026-01-09 17:04:52 +0100

    pag7/17 US3

---
+++ app/Http/Controllers/RevisorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class RevisorController extends Controller
+{
+    public function index()
+    {
+        $article_to_check = Article::where('is_accepted', null)->first();
+        return view('revisor.index', compact('article_to_check'));
+    }
+
+}
