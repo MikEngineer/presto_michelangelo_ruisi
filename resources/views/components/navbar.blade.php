1694258b5ddc8fbb9c27677461dec92dfa453ead
Michelangelo Ruisi <mruisi96@gmail.com>
2026-01-10 15:59:10 +0100

    User Story 10 completata

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -29,6 +29,20 @@
         </ul>
       </li>
       
+      <form class="d-flex ms-auto" role="search" action="{{ route('article.search') }}" method="GET">
+        <div class="input-group">
+          <input
+          type="search"
+          name="query"
+          class="form-control"
+          placeholder="Search"
+          aria-label="search"
+          >
+          <button type="submit" class="input-group-text btn btn-outline-success" id="basic-addon2">Search</button>
+        </div>
+      </form>
+      
+      
       
       
       
@@ -49,14 +63,14 @@
           <a class="dropdown-item" href="{{ route('create.article') }}">Crea</a>
         </li>
         @if (Auth::user()->is_revisor)
-          <li class="nav-item">
-            <a class="nav-link btn btn-outline-success btn-sm position-relative w-sm-25" href="{{ route('revisor.index') }}">Zona revisore
-              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                  {{ \App\Models\Article::toBeRevisedCount() }}
-              </span>
-            </a>
-          </li>
-
+        <li class="nav-item">
+          <a class="nav-link btn btn-outline-success btn-sm position-relative w-sm-25" href="{{ route('revisor.index') }}">Zona revisore
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              {{ \App\Models\Article::toBeRevisedCount() }}
+            </span>
+          </a>
+        </li>
+        
         @endif
       </ul>
     </li>
