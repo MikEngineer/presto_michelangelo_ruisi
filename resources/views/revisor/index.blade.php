e022cad45cd55d2fd3321d8a5fed42360bc5c326
Michelangelo Ruisi <mruisi96@gmail.com>
2026-01-11 18:28:55 +0100

    US 6 completata

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -14,7 +14,7 @@
             @foreach ($article_to_check->images as $key => $image)
                 <div class="col-6 col-md-4 mb-4">
                     <img
-                        src="{{ Storage::url($image->path) }}"
+                        src="{{ $image->getUrl(300, 300) }}"
                         class="img-fluid rounded shadow"
                         alt="Immagine {{ $key + 1 }} dell'articolo {{ $article_to_check->title }}"
                     >
